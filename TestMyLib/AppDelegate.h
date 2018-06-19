535eeb992d815dec96a2273d832e523b118974ec
she <she@192.168.0.103>
2018-06-20 07:33:02 +0800

    Initial Commit

---
+++ TestMyLib/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestMyLib
+//
+//  Created by she on 2018/6/20.
+//  Copyright © 2018年 she. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
